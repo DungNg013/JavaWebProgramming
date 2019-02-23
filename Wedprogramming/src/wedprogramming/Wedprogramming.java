2482424066af02778ba854cc2e5b001c3f005712
DungNg013 <dungz01399@gmail.com>
2019-02-23 17:37:47 +0700

    hoc mang

---
+++ Wedprogramming/src/wedprogramming/Wedprogramming.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wedprogramming;
+
+/**
+ *
+ * @author Dell E5440
+ */
+public class Wedprogramming {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ buoi 5/src/buoi/pkg5/Buoi5.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buoi.pkg5;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Dell E5440
+ */
+public class Buoi5 {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     int[] phantu = new int [5];
+     Scanner sc = new Scanner(System.in);
+     
+     for(int i=0; i<phantu.length; i++){
+         phantu[i] = sc.nextInt();
+     }
+     int tong = 0;
+     for(int j=0; j<phantu.length;j++ ){
+         tong += phantu[j];
+         System.out.println("phantu:" + phantu[j]  );
+     }
+     System.out.println("tong:" + tong  );
+     int tongChan = 0; 
+     int tongLe = 0;
+     for(int f=0; f<phantu.length; f++){
+         if(phantu[f] %2 ==0){
+           tongChan += phantu[f];
+           
+         }
+         else
+         {
+             tongLe += phantu[f];
+         }
+     }
+     System.out.println("tongchan:" + tongChan  );
+     System.out.println("tongle:" + tongLe );
+    }
+    
+  
+//     int max=phantu[1];
+//     int min=phantu[1];
+//     for( int i=0;i<phantu.length;i++){
+//       = phantu[i];  if(phantu[i]>max)
+//             max 
+//      min = phantu[i];
+//      if(phantu[i]<min)
+//             
+//     }
+//     System.out.println(" max = " +max);
+//     System.out.println(" min = " +min);
+    
+}
