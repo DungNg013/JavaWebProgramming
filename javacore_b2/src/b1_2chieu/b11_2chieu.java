63bdfbf4999ece475ed68c16581fc145872e9201
DungNg013 <dungz01399@gmail.com>
2019-03-02 14:29:43 +0700

    mang 2 chieu

--- javacore_b2/src/b1_2chieu/b11_2chieu.java
+++ javacore_b2/src/b1_2chieu/b11_2chieu.java
@@ -14,21 +14,21 @@ import java.util.Scanner;
 public class b11_2chieu {
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
          
-        int arr[][] = new int [3][3];
-        
-       //  for (int i = 0 ; i < 3; i++) {
-        //    for (int j = 0; j < 3; j++) {
-        //        arr[i][j] = sc.nextInt();
-        //    }
-       // }
-         
-       // int arr[][] = {{1, 2}, {3, 4}, {5, 6}};
+//        int arr[][] = new int [3][3];
+//        
+//         for (int i = 0 ; i < 3; i++) {
+//            for (int j = 0; j < 3; j++) {
+//                arr[i][j] = sc.nextInt();
+//            }
+//        }
+//         
+        int arr[][] = {{1, 2}, {3, 4}, {5, 6}};
         
           for (int i = 0 ; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.println(""+arr[i][j]);
+                System.out.println(+arr[i][j]);
             }
           }
     }
